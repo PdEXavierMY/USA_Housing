39b8ca5bc01b8df2a5c20617d00e1e282d7eab4b
Xavitheforce <javier.miguelez.yague@gmail.com>
2022-05-06 11:58:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,3 +4,4 @@ import matplotlib.pyplot as plt
 
 datos = pd.read_csv("USA_Housing.csv")
 print(datos)
+datos.rename(columns={'Avg. Area Income':'Ingresos/area', 'Avg. Area House Age':'Antiguedadcasa/area', 'Avg. Area Number of Rooms':'Habitaciones/area', 'Avg. Area Number of Bedrooms':'Dormitorios/area', 'Population':'Población', 'Price':'Precio', 'Address':'Dirección'})
