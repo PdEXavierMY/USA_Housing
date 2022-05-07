218ca94d020a2c02694c80a29b5f8ac33eca6676
Xavitheforce <javier.miguelez.yague@gmail.com>
2022-05-07 18:06:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ import matplotlib.pyplot as plt
 Housing = pd.read_csv("USA_Housing.csv")
 Housing.rename(columns={'Avg. Area Income':'Ingresos_area', 'Avg. Area House Age':'Antiguedadcasa_area', 'Avg. Area Number of Rooms':'Habitaciones_area', 'Avg. Area Number of Bedrooms':'Dormitorios_area', 'Population':'Población', 'Price':'Precio', 'Address':'Dirección'}, inplace=True)
 Housing.dropna()
-print(Housing)
 mediaingresos_area = Housing['Ingresos_area'].mean()
 mediaprecio = Housing['Precio'].mean()
+
+#print(Housing.info())
+
+def graficos():#luego se separa
+    
